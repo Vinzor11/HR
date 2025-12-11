4140ee2eed5d546634d2147d639dba9fa3867bb9
Vinzor11 <arvinnegrillo4@gmail.com>
2025-12-11 18:07:31 +0800

    Revert "Add Gmail email verification system for user registration"
    This reverts commit db4b7e3721f4936867605c6709fa003a80960df8.

--- routes/auth.php
+++ routes/auth.php
@@ -2,7 +2,6 @@
 
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use App\Http\Controllers\Auth\ConfirmablePasswordController;
-use App\Http\Controllers\Auth\EmailVerificationCodeController;
 use App\Http\Controllers\Auth\EmailVerificationNotificationController;
 use App\Http\Controllers\Auth\EmailVerificationPromptController;
 use App\Http\Controllers\Auth\NewPasswordController;
@@ -17,18 +16,7 @@ Route::middleware('guest')->group(function () {
     Route::get('register', [RegisteredUserController::class, 'create'])
         ->name('register');
 
-    // Registration now sends verification code first
-    Route::post('register', [EmailVerificationCodeController::class, 'sendCode']);
-    
-    // Email verification code routes
-    Route::get('verify-email-code', [EmailVerificationCodeController::class, 'show'])
-        ->name('verification.code.show');
-    
-    Route::post('verify-email-code', [EmailVerificationCodeController::class, 'verify'])
-        ->name('verification.code.verify');
-    
-    Route::post('verify-email-code/resend', [EmailVerificationCodeController::class, 'resend'])
-        ->name('verification.code.resend');
+    Route::post('register', [RegisteredUserController::class, 'store']);
 
     Route::get('login', [AuthenticatedSessionController::class, 'create'])
         ->name('login');
