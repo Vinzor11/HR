3427063dc5783963ca47cfd43792b585e0b88ac8
Vinzor11 <arvinnegrillo4@gmail.com>
2025-12-13 07:45:49 +0800

    feat: Update landing page background to use landing-page.png
    - Moved landing page.png from storage/tmp to public/images
    - Renamed file to landing-page.png for web compatibility
    - Updated welcome.tsx to use new background image
    - Maintained all existing styling and overlay effects
    
    The landing page now uses the new landing-page.png background image

--- resources/js/pages/welcome.tsx
+++ resources/js/pages/welcome.tsx
@@ -11,7 +11,7 @@ export default function Welcome() {
                 <link rel="preconnect" href="https://fonts.bunny.net" />
                 <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
             </Head>
-            <div className="flex min-h-screen flex-col items-center bg-cover bg-[center_top] bg-no-repeat text-[#1b1b18] lg:justify-center relative" style={{backgroundImage: 'url("/images/welcome-essu.png")'}}>
+            <div className="flex min-h-screen flex-col items-center bg-cover bg-[center_top] bg-no-repeat text-[#1b1b18] lg:justify-center relative" style={{backgroundImage: 'url("/images/landing-page.png")'}}>
                 <div className="absolute inset-0 bg-white/60 dark:bg-black/50"></div>
 
                 {/* Top right navigation */}
